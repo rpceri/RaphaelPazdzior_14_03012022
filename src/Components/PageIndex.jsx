a214808e624cfa4bb64b19e1a1e5e3ad0f2a2b0e
Raphael Pazdzior <rpazdzior@ceri-informatique.fr>
2022-01-04 13:37:00 +0100

    fix: css of index page

--- src/Components/PageIndex.jsx
+++ src/Components/PageIndex.jsx
@@ -12,7 +12,67 @@ import { Link } from 'react-router-dom';
 */
 function PageIndex() {
     const departments = ['Sales', 'Marketing', 'Engineering', 'Human Ressources', 'Legal'];
-    const states = ['Alabama', 'Alaska', 'American Samoa', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'District of Columbia', 'Federated States of Micronesia', 'Florida', 'Georgia', 'Guam', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Marshall Islands', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Northern Mariana Islands', 'Ohio', 'Oklahoma', 'Oregon', 'Palau', 'Pennsylvania', 'Puerto Rico', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virgin Island', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'];
+    const states = [
+        {"name": "Alabama", "abbreviation": "AL"},
+        {"name": "Alaska", "abbreviation": "AK"},
+        {"name": "American Samoa", "abbreviation": "AS"},
+        {"name": "Arizona", "abbreviation": "AZ"},
+        {"name": "Arkansas", "abbreviation": "AR"},
+        {"name": "California", "abbreviation": "CA"},
+        {"name": "Colorado", "abbreviation": "CO"},
+        {"name": "Connecticut", "abbreviation": "CT"},
+        {"name": "Delaware", "abbreviation": "DE"},
+        {"name": "District Of Columbia", "abbreviation": "DC"},
+        {"name": "Federated States Of Micronesia", "abbreviation": "FM"},
+        {"name": "Florida", "abbreviation": "FL"},
+        {"name": "Georgia", "abbreviation": "GA"},
+        {"name": "Guam", "abbreviation": "GU"},
+        {"name": "Hawaii", "abbreviation": "HI"},
+        {"name": "Idaho", "abbreviation": "ID"},
+        {"name": "Illinois", "abbreviation": "IL"},
+        {"name": "Indiana", "abbreviation": "IN"},
+        {"name": "Iowa", "abbreviation": "IA"},
+        {"name": "Kansas", "abbreviation": "KS"},
+        {"name": "Kentucky", "abbreviation": "KY"},
+        {"name": "Louisiana", "abbreviation": "LA"},
+        {"name": "Maine", "abbreviation": "ME"},
+        {"name": "Marshall Islands", "abbreviation": "MH"},
+        {"name": "Maryland", "abbreviation": "MD"},
+        {"name": "Massachusetts", "abbreviation": "MA"},
+        {"name": "Michigan", "abbreviation": "MI"},
+        {"name": "Minnesota", "abbreviation": "MN"},
+        {"name": "Mississippi", "abbreviation": "MS"},
+        {"name": "Missouri", "abbreviation": "MO"},
+        {"name": "Montana", "abbreviation": "MT"},
+        {"name": "Nebraska", "abbreviation": "NE"},
+        {"name": "Nevada", "abbreviation": "NV"},
+        {"name": "New Hampshire", "abbreviation": "NH"},
+        {"name": "New Jersey", "abbreviation": "NJ"},
+        {"name": "New Mexico", "abbreviation": "NM"},
+        {"name": "New York", "abbreviation": "NY"},
+        {"name": "North Carolina", "abbreviation": "NC"},
+        {"name": "North Dakota", "abbreviation": "ND"},
+        {"name": "Northern Mariana Islands", "abbreviation": "MP"},
+        {"name": "Ohio", "abbreviation": "OH"},
+        {"name": "Oklahoma", "abbreviation": "OK"},
+        {"name": "Oregon", "abbreviation": "OR"},
+        {"name": "Palau", "abbreviation": "PW"},
+        {"name": "Pennsylvania", "abbreviation": "PA"},
+        {"name": "Puerto Rico", "abbreviation": "PR"},
+        {"name": "Rhode Island", "abbreviation": "RI"},
+        {"name": "South Carolina", "abbreviation": "SC"},
+        {"name": "South Dakota", "abbreviation": "SD"},
+        {"name": "Tennessee", "abbreviation": "TN"},
+        {"name": "Texas", "abbreviation": "TX"},
+        {"name": "Utah", "abbreviation": "UT"},
+        {"name": "Vermont", "abbreviation": "VT"},
+        {"name": "Virgin Islands", "abbreviation": "VI"},
+        {"name": "Virginia", "abbreviation": "VA"},
+        {"name": "Washington", "abbreviation": "WA"},
+        {"name": "West Virginia", "abbreviation": "WV"},
+        {"name": "Wisconsin", "abbreviation": "WI"},
+        {"name": "Wyoming", "abbreviation": "WY"}
+    ];
     
     const [firstName, setFirstName] = useState('');
     const [lastName, setLastName] = useState('');
@@ -24,9 +84,6 @@ function PageIndex() {
     const [zip, setZip] = useState('');
     const [department, setDepartment] = useState('');
 
-    const [resetBirth, setResetBirth] = useState(Math.random());
-    const [resetStart, setResetstart] = useState(Math.random());
-    const [reset, setReset] = useState(false);
     const [error, setError] = useState(false);
     const [addEmployeeMessage, setAddEmployeeMessage] = useState('');
 
@@ -88,10 +145,7 @@ function PageIndex() {
             setStateError('');
             setZipError('');
             setDepartmentError('');
-        
-            setResetBirth(Math.random());
-            setResetstart(Math.random());
-            setReset(!reset);
+
             setFirstName('');
             setLastName('');
             setStreet('');
@@ -100,7 +154,8 @@ function PageIndex() {
         }
     }
 
-    return ( <>
+    return ( 
+    <>
         <div className="title">
             <h1>HR.net</h1>
         </div>
@@ -145,7 +200,7 @@ function PageIndex() {
 
                 </div>
             </form>
-            <button onClick={handleSubmit}>Save</button>
+            <button onClick={handleSubmit}>Save</ button>
         </div>
 
     </>
